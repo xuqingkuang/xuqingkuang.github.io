26610cabcbaf8119998fe5aa427e5aac7ecf087b
XQ Kuang <x@kxq.io>
2016-07-12 15:58:49 +0800

    * Changed to use createdAt order

--- src/models/blog-query.js
+++ src/models/blog-query.js
@@ -1,5 +1,5 @@
 import AV from 'leancloud-storage';
 
 const blogQuery = new AV.Query('Blog');
-blogQuery.limit(1000).descending('updatedAt');
+blogQuery.limit(1000).descending('createdAt');
 export default blogQuery;
